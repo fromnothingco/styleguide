80d0b31a895d6b1f9a46ee286c99e7b634b3c26f
jonathan hamilton <jonathanhamilton@jonathans-MacBook-Pro-2.local>
2019-08-29 14:02:03 +0100

    added validation on inputs

--- src/components/generics/inputs/input.js
+++ src/components/generics/inputs/input.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import styled from "styled-components";
 import PropTypes from "prop-types";
 import Label from "../label";
@@ -7,19 +7,47 @@ const InputStyle = styled.input`
   display: block;
   width: 100%;
   box-sizing: border-box;
+  font-size: 16px;
   border: ${({ theme }) => theme.input.border || "1px solid #ccc"};
   padding: ${({ theme }) => theme.input.padding || "0.5rem"};
   margin: ${({ theme }) => theme.input.margin || "5px 0"};
   background: ${({ theme }) => theme.input.background || "#fff"};
   border-radius: ${({ theme }) => theme.input.borderRadius || "5px"};
+  &.error {
+    border-color: ${({ theme }) => theme.colors.danger};
+  }
+  &.valid {
+    border-color: ${({ theme }) => theme.colors.success};
+  }
 `;
 
+const Message = styled.div``;
+
 const Input = props => {
-  const { label, name } = props;
+  const inputhere = React.createRef();
+  const [error, setError] = useState(null);
+  const { label, name, placehodler = null } = props;
+
+  const validate = () => {
+    console.log(inputhere.current.validity);
+    if (!inputhere.current.validity.valid) {
+      setError("error");
+    } else {
+      setError("valid");
+    }
+  };
+
   return (
     <div>
       <Label htmlFor={name}>{label}</Label>
-      <InputStyle {...props} id={name} />
+      <InputStyle
+        ref={inputhere}
+        {...props}
+        className={error}
+        id={name}
+        placeholder={placehodler}
+        onBlur={validate}
+      />
     </div>
   );
 };
---
+++ src/components/headings/index.js
@@ -0,0 +1,13 @@
+import { H2 } from "../generics/headings";
+import styled from "styled-components";
+
+const Heading = styled(H2)`
+  padding: 15px 0;
+  margin: 30px 0;
+  font-size: 1.2rem;
+  font-weight: bold;
+  border-bottom: 1px solid #eaeaea;
+  border-top: 1px solid #eaeaea;
+`;
+
+export default Heading;
